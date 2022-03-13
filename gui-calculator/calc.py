2540055e5349ab1dfbdc8948b8255156389057bc
fluxt <100445187+fluxtt@users.noreply.github.com>
2022-03-13 23:18:18 +0000

    Create calc.py

---
+++ gui-calculator/calc.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+
+__version__ = '0.1'
+__author__ = 'Fluxt'
+
+class PyCalcUi(QMainWindow):
+    def __init__(self):
