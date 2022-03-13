a545521061f7cf0080d67e120f8420fc0601ab9b
fluxt <100445187+fluxtt@users.noreply.github.com>
2022-03-13 23:21:44 +0000

    Update calc.py

--- gui-calculator/calc.py
+++ gui-calculator/calc.py
@@ -9,3 +9,17 @@ __author__ = 'Fluxt'
 
 class PyCalcUi(QMainWindow):
     def __init__(self):
+        super().__init__()
+        self.setWindowTitle('PyCalc')
+        self.setFixedSize(235, 235)
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+
+def main():
+    pycalc = QApplication(sys.argv)
+    view = PyCalcUi()
+    view.show()
+    sys.exit(pycalc.exec_())
+
+if __name__ == '__main__':
+    main()
